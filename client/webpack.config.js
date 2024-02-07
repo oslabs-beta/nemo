021e5378b712e9ed13a1d9aca0966c679c1419f5
Paul Burger <iniidabyss@gmail.com>
2024-02-07 11:29:52 -0500

    Uploading initial boilerplate

---
+++ client/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js',
+  },
+
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.s?css$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader', 'sass-loader',],
+      },
+    ],
+  },
+};
