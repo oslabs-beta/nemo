f0ea553f4f2100abf6fac27b5683753bdf92d313
Paul Burger <iniidabyss@gmail.com>
2024-02-10 21:27:41 -0500

    Added additional functionality to getMemory method on nodeExporterController. Now returns object with available memory, total memory and percent memory used, per node

--- server/controllers/nodeExporterController.js
+++ server/controllers/nodeExporterController.js
@@ -26,35 +26,52 @@ nodeExporterController.getMetrics = async (req, res, next) => {
 };
 
 nodeExporterController.getMemory = (req, res, next) => {
+  // returns various memory metrics for single node as an object
+  // all returned numeric values in bytes, except for perUsed in percent
+  const memObj = {};
+
   // regex expression for memory (bytes) usage in http stream
-  const regex = /node_memory_MemTotal_bytes\s+\d+(\.\d+)?(e[+\-]?\d+)?/;
-  const match = res.locals.getMetrics.match(regex);
-  
-  let tempNum;
-  let exp;
-  
-  if (!match) {
+  // MemAvailable is the metric to check for available memory, NOT MemFree !
+  const regxTotMem = /node_memory_MemTotal_bytes\s+\d+(\.\d+)?(e[+\-]?\d+)?/;
+  const regxMemAvail =
+    /node_memory_MemAvailable_bytes\s+\d+(\.\d+)?(e[+\-]?\d+)?/;
+
+  const getMeMetric = (regExp) => {
+    const match = res.locals.getMetrics.match(regExp);
+    let tempNum;
+    let exponent;
+
+    if (!match) return null;
+    // get index of space in regex match[0]
+    const spaceIdx = match[0].indexOf(' ');
+    // slice everything after the space to get the number of interest
+    tempNum = match[0].slice(spaceIdx + 1);
+    // get index of where 'e' occurs in sliced string
+    const eIdx = tempNum.indexOf('e');
+    // read exponent into numeric variable
+    exponent = Number(tempNum.slice(eIdx + 2));
+    // to get the actual number, slice everything up to where exponent is declared
+    tempNum = Number(tempNum.slice(0, eIdx));
+    // multiply tempNum by exponent to get relevent mem number in bytes
+    return (tempNum = tempNum * 10 ** exponent);
+  };
+
+  // total memory on node in bytes
+  memObj.total = getMeMetric(regxTotMem);
+  // total available memory on node in bytes
+  memObj.avail = getMeMetric(regxMemAvail);
+
+  if (!memObj.total || !memObj.avail) {
     return next({
       log: `Express error handler caught in get memory controller: ${err}`,
       status: 404,
       message: { err: 'Memory Information Not Found' },
     });
-
-  } else {
-    // get number from match[0] element, including exponent at end
-    const spaceIdx = match[0].indexOf(' ');
-    tempNum = match[0].slice(spaceIdx + 1);
-    // read exponent from tempNum string, coerce to number
-    const eIdx = tempNum.indexOf('e');
-    exp = Number(tempNum.slice(eIdx + 2));
-    // remove exponent from tempNum string, coerce to number
-    tempNum = Number(tempNum.slice(0, eIdx));
-    
-    // calculated # of bytes
-    tempNum = (tempNum * 10**exp).toString();
   }
-  res.locals.memory = tempNum;
 
+  // percent of memory used on node
+  memObj.perUsed = ((memObj.total - memObj.avail) / memObj.total) * 100;
+  res.locals.memory = memObj;
   return next();
 };
 
