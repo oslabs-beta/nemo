394e966b68868d4960395dc30df635787d884e3f
Trevor <trevorhilz@gmail.com>
2024-02-13 14:01:24 -0600

    added flex styling and GB metric conversion on a few line items.

--- client/src/components/NodeSummary.jsx
+++ client/src/components/NodeSummary.jsx
@@ -25,17 +25,33 @@ const NodeSummary = () => {
           setIntIp(data[0].status.addresses[0].address); // Internal IP
           setExtIp(data[0].status.addresses[1].address); // External IP
           setClusterCPU(data[0].status.capacity.cpu); // Cluster CPU
-          setClusterEphStorage(data[0].status.capacity['ephemeral-storage']);
-          setClusterMemory(data[0].status.capacity.memory); // Cluster Memory
+          setClusterEphStorage(
+            (
+              Number(
+                data[0].status.capacity['ephemeral-storage'].slice(0, -2)
+              ) / 976562.5
+            ).toFixed(2)
+          );
+          setClusterMemory(
+            Number(
+              data[0].status.capacity.memory.slice(0, -2) / 976562.5
+            ).toFixed(2)
+          ); // Cluster Memory
           setClusterPods(data[0].status.capacity.pods); // Cluster Pods
-          setClusterCPUAlloc(Number((data[0].status.allocatable.cpu).slice(0, -1)) / 1000); // Cluster CPU
+          setClusterCPUAlloc(
+            Number(data[0].status.allocatable.cpu.slice(0, -1)) / 1000
+          ); // Cluster CPU
           setClusterEphStorageAlloc(
             data[0].status.allocatable['ephemeral-storage']
           );
-          setClusterMemoryAlloc(data[0].status.allocatable.memory); // Cluster Memory
+          setClusterMemoryAlloc(
+            Number(
+              data[0].status.allocatable.memory.slice(0, -2) / 976562.5
+            ).toFixed(2)
+          ); // Cluster Memory
           setClusterPodsAlloc(data[0].status.allocatable.pods); // Cluster Pods
           console.log('Node Info: ', data[0].status.nodeInfo);
-          console.log('allocatable: ', data[0].status.allocatable);
+          // console.log('allocatable: ', data[0].status.allocatable);
           // console.log('table 3: ', data[0].status.images);
           // console.table('table 4: ', data[0].metadata.managedFields);
           // console.log('data: ', data);
@@ -44,55 +60,59 @@ const NodeSummary = () => {
       fetch('http://localhost:3000/nodeExporter/memory', {})
         .then((data) => data.json())
         .then((data) => {
-          console.log('Node Exporter: ', data);
+          // console.log('Node Exporter: ', data);
           setNodeMemoryTotal((data.total / 1000000000).toFixed(2));
           setNodeMemoryAvail((data.avail / 1000000000).toFixed(2));
-          setNodeMemoryPercUsed(data.perUsed.toFixed(4));
+          setNodeMemoryPercUsed(data.perUsed.toFixed(3));
         });
     }, 2000);
   }, []);
 
   return (
-    <div
-      style={{
-        border: 'solid #ffffff 1px',
-        borderRadius: '15px',
-        padding: '10px',
-        width: '400px',
-      }}
-    >
+    <div style={{ display: 'flex', justifyContent: 'center' }}>
       <div
         style={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
+          border: 'solid #ffffff 1px',
+          borderRadius: '15px',
+          padding: '10px',
+          width: '400px',
         }}
       >
-        <table>
-          <th>Node Summary</th>
-          <tr></tr>
-          <th>IP ADDRESSES</th>
-          <tr>Internal IP: {intIp}</tr>
-          <tr>External IP: {extIp}</tr>
-          <tr>Node ID: {UID}</tr>
-          <th>CLUSTER INFORMATION</th>
-          <tr>CPU Capacity: {clusterCPU}</tr>
-          <tr>CPU Allocatable: {clusterCPUAlloc}</tr>
-          <tr>Ephemeral Storage Capacity: {clusterEphStorage}</tr>
-          <tr>Ephemeral Storage Allocatable: {clusterEphStorageAlloc}</tr>
-          <tr>Memory Capacity: {clusterMemory}</tr>
-          <tr>Memory Allocatable: {clusterMemoryAlloc}</tr>
-          <tr>Pods Capacity: {clusterPods}</tr>
-          <tr>Pods Allocatable: {clusterPodsAlloc}</tr>
-          <th>NODE MEMORY</th>
-          <tr>Node Memory Total: {nodeMemoryTotal} GB</tr>
-          <tr>Node Memory Available: {nodeMemoryAvail} GB</tr>
-          <tr>Node Memory Percent Used: {nodeMemoryPercUsed}%</tr>
-        </table>
-      </div>
-      <div style={{ padding: '10px', display: 'flex' }}>
-        <button>Button 1</button>
-        <button>Button 2</button>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <table>
+            <th>Node Summary</th>
+            <tr></tr>
+            <th>IP ADDRESSES</th>
+            <tr>Internal IP: {intIp}</tr>
+            <tr>External IP: {extIp}</tr>
+            <tr>Node ID: {UID}</tr>
+            <th>CLUSTER INFORMATION</th>
+            <tr>CPU Capacity: {clusterCPU}</tr>
+            <tr>CPU Allocatable: {clusterCPUAlloc}</tr>
+            <tr>Ephemeral Storage Capacity: {clusterEphStorage} GB</tr>
+            <tr>Ephemeral Storage Allocatable: {clusterEphStorageAlloc} GB</tr>
+            <tr>Memory Capacity: {clusterMemory} GB</tr>
+            <tr>Memory Allocatable: {clusterMemoryAlloc} GB</tr>
+            <tr>Pods Capacity: {clusterPods}</tr>
+            <tr>Pods Allocatable: {clusterPodsAlloc}</tr>
+            <th>NODE MEMORY</th>
+            <tr>Node Memory Total: {nodeMemoryTotal} GB</tr>
+            <tr>Node Memory Available: {nodeMemoryAvail} GB</tr>
+            <tr>Node Memory Percent Used: {nodeMemoryPercUsed}%</tr>
+          </table>
+        </div>
+        <div
+          style={{ padding: '10px', display: 'flex', justifyContent: 'center' }}
+        >
+          <button>Pods</button>
+          <button>Services</button>
+        </div>
       </div>
     </div>
   );
