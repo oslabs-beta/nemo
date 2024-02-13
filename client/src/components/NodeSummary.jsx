2e7b8746c7d5ecc0ef094bbc8a758b46d925c930
Trevor <trevorhilz@gmail.com>
2024-02-13 11:36:00 -0600

    state refreshes now, interval is set to 2000ms. Added more line items to the frontend cards.

--- client/src/components/NodeSummary.jsx
+++ client/src/components/NodeSummary.jsx
@@ -2,50 +2,63 @@ import React, { useEffect, useState } from 'react';
 
 const NodeSummary = () => {
   const [UID, setUID] = useState('');
+  const [extIp, setExtIp] = useState('');
+  const [intIp, setIntIp] = useState('');
+  const [clusterCPU, setClusterCPU] = useState('');
+  const [clusterEphStorage, setClusterEphStorage] = useState('');
+  const [clusterMemory, setClusterMemory] = useState('');
+  const [clusterPods, setClusterPods] = useState('');
+  const [clusterCPUAlloc, setClusterCPUAlloc] = useState('');
+  const [clusterEphStorageAlloc, setClusterEphStorageAlloc] = useState('');
+  const [clusterMemoryAlloc, setClusterMemoryAlloc] = useState('');
+  const [clusterPodsAlloc, setClusterPodsAlloc] = useState('');
   const [nodeMemoryTotal, setNodeMemoryTotal] = useState('');
   const [nodeMemoryAvail, setNodeMemoryAvail] = useState('');
   const [nodeMemoryPercUsed, setNodeMemoryPercUsed] = useState('');
 
   useEffect(() => {
-    fetch('http://localhost:3000/metricserver/nodes', {
-      // mode: 'cors',
-      // headers: {
-      //   'Access-Control-Allow-Origin': '*',
-      // },
-    })
-      .then((data) => data.json())
-      .then((data) => {
-        console.log('Node Info: ', data[0].status.nodeInfo);
-        console.log(data[0].status.addresses);
-        console.log(data[0].status.capacity);
-        console.log(data[0].status.images);
-        setUID(data[0].metadata.uid);
-        console.table(data[0].metadata.managedFields);
-        console.log(data);
-      });
+    setInterval(() => {
+      fetch('http://localhost:3000/metricserver/nodes', {})
+        .then((data) => data.json())
+        .then((data) => {
+          setUID(data[0].metadata.uid); // Node ID
+          setIntIp(data[0].status.addresses[0].address); // Internal IP
+          setExtIp(data[0].status.addresses[1].address); // External IP
+          setClusterCPU(data[0].status.capacity.cpu); // Cluster CPU
+          setClusterEphStorage(data[0].status.capacity['ephemeral-storage']);
+          setClusterMemory(data[0].status.capacity.memory); // Cluster Memory
+          setClusterPods(data[0].status.capacity.pods); // Cluster Pods
+          setClusterCPUAlloc(Number((data[0].status.allocatable.cpu).slice(0, -1)) / 1000); // Cluster CPU
+          setClusterEphStorageAlloc(
+            data[0].status.allocatable['ephemeral-storage']
+          );
+          setClusterMemoryAlloc(data[0].status.allocatable.memory); // Cluster Memory
+          setClusterPodsAlloc(data[0].status.allocatable.pods); // Cluster Pods
+          console.log('Node Info: ', data[0].status.nodeInfo);
+          console.log('allocatable: ', data[0].status.allocatable);
+          // console.log('table 3: ', data[0].status.images);
+          // console.table('table 4: ', data[0].metadata.managedFields);
+          // console.log('data: ', data);
+        });
 
-    fetch('http://localhost:3000/nodeExporter/memory', {
-      // mode: 'cors',
-      // headers: {
-      //   'Access-Control-Allow-Origin': '*',
-      // },
-    })
-      .then((data) => data.json())
-      .then((data) => {
-        console.log('Node Exporter: ', data);
-        setNodeMemoryTotal((data.total / 1000000000).toFixed(2));
-        setNodeMemoryAvail((data.avail / 1000000000).toFixed(2));
-        setNodeMemoryPercUsed(data.perUsed.toFixed(2));
-      });
+      fetch('http://localhost:3000/nodeExporter/memory', {})
+        .then((data) => data.json())
+        .then((data) => {
+          console.log('Node Exporter: ', data);
+          setNodeMemoryTotal((data.total / 1000000000).toFixed(2));
+          setNodeMemoryAvail((data.avail / 1000000000).toFixed(2));
+          setNodeMemoryPercUsed(data.perUsed.toFixed(4));
+        });
+    }, 2000);
   }, []);
 
   return (
     <div
       style={{
-        border: 'solid black 1px',
+        border: 'solid #ffffff 1px',
         borderRadius: '15px',
         padding: '10px',
-        width: '300px',
+        width: '400px',
       }}
     >
       <div
@@ -57,7 +70,21 @@ const NodeSummary = () => {
       >
         <table>
           <th>Node Summary</th>
+          <tr></tr>
+          <th>IP ADDRESSES</th>
+          <tr>Internal IP: {intIp}</tr>
+          <tr>External IP: {extIp}</tr>
           <tr>Node ID: {UID}</tr>
+          <th>CLUSTER INFORMATION</th>
+          <tr>CPU Capacity: {clusterCPU}</tr>
+          <tr>CPU Allocatable: {clusterCPUAlloc}</tr>
+          <tr>Ephemeral Storage Capacity: {clusterEphStorage}</tr>
+          <tr>Ephemeral Storage Allocatable: {clusterEphStorageAlloc}</tr>
+          <tr>Memory Capacity: {clusterMemory}</tr>
+          <tr>Memory Allocatable: {clusterMemoryAlloc}</tr>
+          <tr>Pods Capacity: {clusterPods}</tr>
+          <tr>Pods Allocatable: {clusterPodsAlloc}</tr>
+          <th>NODE MEMORY</th>
           <tr>Node Memory Total: {nodeMemoryTotal} GB</tr>
           <tr>Node Memory Available: {nodeMemoryAvail} GB</tr>
           <tr>Node Memory Percent Used: {nodeMemoryPercUsed}%</tr>
