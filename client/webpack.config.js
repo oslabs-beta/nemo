3906135f1dec80abf37c9667239145bd87a6f5c3
callummiles <cwmiles18@gmail.com>
2024-02-19 15:32:43 -0600

    tailwind set up complete, no styling yet

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
       {
         test: /\.s?css$/,
         exclude: /node_modules/,
-        use: ['style-loader', 'css-loader', 'sass-loader',],
+        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
       },
     ],
   },
