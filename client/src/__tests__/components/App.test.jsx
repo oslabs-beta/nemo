b96dac144817a5fbe5d081109ea6ddfbd6ba3061
Paul Burger <iniidabyss@gmail.com>
2024-07-01 13:34:33 -0400

    Added additional test to App.test

--- client/src/__tests__/components/App.test.jsx
+++ client/src/__tests__/components/App.test.jsx
@@ -2,9 +2,28 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import App from "../../components/App";
+import NodeContainer from "../../components/NodeContainer";
 
 test("renders Welcome to Nemo!", () => {
   render(<App />);
   const linkElement = screen.getByText("Welcome to Nemo!");
   expect(linkElement).toBeInTheDocument();
 });
+
+test("renders What Is Nemo?", () => {
+  render(<App />);
+
+  //console.log(screen.debug);
+  const linkElement2 = screen.getByText("What Is Nemo?");
+  expect(linkElement2).toBeInTheDocument();
+});
+
+// test("renders Nodes table", async () => {
+//   await
+
+//   render(<NodeContainer />);
+
+//   //console.log(screen.debug);
+//   const tableComp = screen.getByRole("table");
+//   expect(tableComp).toBeInTheDocument();
+// });
